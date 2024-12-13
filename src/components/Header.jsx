fd08fd60b78798123c014ceec136f4b789ab5c38
revathi <arevathi125@gmail.com>
2024-12-13 11:53:09 -0600

    committed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import { Bars3Icon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 export default function Header() {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <header className="flex justify-between px-5 py-2 bg-primary">
       <a className="font-bold text-black" href="#">
@@ -21,7 +23,27 @@ export default function Header() {
           </li>
         </ul>
       </nav>
-      <button className="block md:hidden">
+      {toggleMenu && (
+        <nav className="block md:hidden">
+          <ul className="flex flex-col text-white mobile-nav">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      )}
+      <button
+        onClick={() => setToggleMenu(!toggleMenu)}
+        className="block md:hidden">
         <Bars3Icon className="text-white h-5" />
       </button>
     </header>
