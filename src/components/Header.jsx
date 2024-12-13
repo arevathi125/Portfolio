460a8ffccaf832831ba271f2b504317357695807
revathi <arevathi125@gmail.com>
2024-12-13 11:26:30 -0600

    committed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,29 @@
+import { Bars3Icon } from "@heroicons/react/24/solid";
 export default function Header() {
   return (
-    <header className="flex justify-between px-5">
-      <a href="#">Revathi</a>
-      <ul className="flex">
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Projects</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
+    <header className="flex justify-between px-5 py-2 bg-primary">
+      <a className="font-bold text-black" href="#">
+        Revathi
+      </a>
+      <nav className="hidden md:block">
+        <ul className="flex text-white">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <button className="block md:hidden">
+        <Bars3Icon className="text-white h-5" />
+      </button>
     </header>
   );
 }
