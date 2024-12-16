3ffce885bd6eb7fb7d2aa40e8622b56ac08c3bc7
revathi <arevathi125@gmail.com>
2024-12-16 15:43:42 -0600

    committed

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import HeroImg from "./";
+
+export default function Hero() {
+  return (
+    <section>
+      <h1>
+        Hi, <br />I am Revathi
+        <p>I am a Full-Stack Developer</p>
+      </h1>
+      {/* <img src="" */}
+    </section>
+  );
+}
