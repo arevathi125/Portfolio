b52a76ff15ea96ee05d3aafa9e969f122f460712
revathi <arevathi125@gmail.com>
2024-12-16 15:55:14 -0600

    committed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,13 @@
-import HeroImg from "./";
+import HeroImg from "../assets/Hero.png";
 
 export default function Hero() {
   return (
-    <section>
-      <h1>
+    <section className="flex">
+      <h1 className="w-1/2">
         Hi, <br />I am Revathi
         <p>I am a Full-Stack Developer</p>
       </h1>
-      {/* <img src="" */}
+      <img className="w-1/2" src={HeroImg}></img>
     </section>
   );
 }
