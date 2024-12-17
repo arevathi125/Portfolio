bd0dd2f18d5e06b12e15187c464701a2003e86fa
revathi <arevathi125@gmail.com>
2024-12-17 15:26:58 -0600

    committed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,22 @@
 import HeroImg from "../assets/Hero.png";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
 
 export default function Hero() {
   return (
     <section className="flex px-5 py-32 bg-secondary justify-center">
-      <div className="md:w-1/2 flex flex-col">
-        <h1 className=" text-white text-5xl">
+      <div className="w-1/2 flex flex-col">
+        <h1 className="text-white text-5xl">
           Hi, <br />I am Revathi
           <p className="text-2xl">I am a Full-Stack Developer</p>
         </h1>
-        <div>
-          <a href="#"></a>
+        <div className="flex py-7">
+          <a href="#" className="pr-5 hover:text-white">
+            <AiOutlineGithub size={40} />
+          </a>
+          <a href="#" className="pr-5 hover:text-white">
+            <AiOutlineLinkedin size={40} />
+          </a>
         </div>
       </div>
       <img className="w-1/3" src={HeroImg}></img>
