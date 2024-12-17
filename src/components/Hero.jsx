da0cd315f7e66228222432a6b4fe3b5d4e6ae85c
revathi <arevathi125@gmail.com>
2024-12-17 15:17:01 -0600

    committed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,15 @@ import HeroImg from "../assets/Hero.png";
 export default function Hero() {
   return (
     <section className="flex px-5 py-32 bg-secondary justify-center">
-      <h1 className="md:w-1/2 text-white text-4xl">
-        Hi, <br />I am Revathi
-        <p className="text-2xl">I am a Full-Stack Developer</p>
-      </h1>
+      <div className="md:w-1/2 flex flex-col">
+        <h1 className=" text-white text-5xl">
+          Hi, <br />I am Revathi
+          <p className="text-2xl">I am a Full-Stack Developer</p>
+        </h1>
+        <div>
+          <a href="#"></a>
+        </div>
+      </div>
       <img className="w-1/3" src={HeroImg}></img>
     </section>
   );
