aa82156346df3ad50b5608bbb210b3942b7fc401
revathi <arevathi125@gmail.com>
2024-12-20 13:33:07 -0600

    committed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,11 +3,11 @@ import AboutImg from "../assets/about.jpg";
 export default function About() {
   return (
     <>
-      <section className="flex bg-secondary">
-        <div>
-          <img className="w-80" src={AboutImg} />
+      <section className="flex flex-col md:flex-row bg-secondary">
+        <div className="md:w-80">
+          <img src={AboutImg} />
         </div>
-        <div className="w-1/2 flex justify-center">
+        <div className="md:w-1/2 flex justify-center">
           <div className="flex flex-col justify-center px-5 py-142">
             <h1 className="text-4xl border-primary text-white border-b-4 mb-5 w-[170px] font-bold">
               About Me
