86a86d3684ae9e0b8d1293fea40353aefd2cb807
revathi <arevathi125@gmail.com>
2025-02-06 14:09:55 -0600

    committed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { AiOutlineGithub } from "react-icons/ai";
 
 export default function Hero() {
   return (
-    <section className="flex  flex-col md:flex-row px-5 py-32 bg-primary justify-center">
+    <section className="flex flex-col md:flex-row px-5 py-32 bg-primary justify-center">
       <div className="md:w-1/2 flex flex-col justify-center">
         <h1 className="text-white text-5xl">
           Hi, <br />I am Revathi
