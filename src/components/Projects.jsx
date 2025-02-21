aef9791dd8076e11ca24d4bee80ad4279b11cc28
revathi <arevathi125@gmail.com>
2025-02-20 19:53:28 -0600

    committed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,8 @@
 export default function Projects() {
   return (
-    <section>
-      <div>Heading</div>
-      <div>Images</div>
+    <section className="flx py-5 px-5 justify-center">
+      <div className="w-1/2">Heading</div>
+      <div className="w-1/2">Images</div>
     </section>
   );
 }
