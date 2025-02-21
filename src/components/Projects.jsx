bc9dca349e7354671de2b679dc1a6897c4f943ce
revathi <arevathi125@gmail.com>
2025-02-20 20:23:28 -0600

    committed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,17 @@
+import bandImg from "../assets/band.jpg";
 export default function Projects() {
   return (
-    <section className="flex py-5 px-5 justify-center bg-primary text-white">
+    <section className="flex py-20 px-5 justify-center bg-primary text-white">
       <div className="w-1/2">
-        <h1 className="text-4xl border-secondary border-b-4 mb-5 w-[160px] font-bold">
-          Projects
-        </h1>
+        <div className="flex justify-center">
+          <h1 className="text-4xl border-secondary border-b-4 mb-5 w-[140px] font-bold">
+            Projects
+          </h1>
+        </div>
       </div>
-      <div className="w-1/2">Images</div>
+      <div className="w-1/2">
+        <img src={bandImg} />
+      </div>
     </section>
   );
 }
