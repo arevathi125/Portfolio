dcce3a5d79a65dce6e87b08d56feea360e3d4186
revathi <arevathi125@gmail.com>
2025-02-21 17:51:18 -0600

    committed

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,25 +4,20 @@ export default function Resume() {
   return (
     <>
       <section className="flex flex-col md:flex-row bg-secondary">
-        <div className="md:w-80 py-5">
-          <img src={ResumeImg} />
+        <div className="md:w-80 py-5" flex justify-end>
+          <img className="w-[300px]" src={ResumeImg} />
         </div>
         <div className="md:w-1/2 flex justify-center">
           <div className="flex flex-col justify-center px-5 py-142">
-            <h1 className="text-4xl border-primary text-white border-b-4 mb-5 w-[170px] font-bold">
-              About Me
+            <h1 className="text-4xl border-primary text-white border-b-4 mb-5 w-[130px] font-bold">
+              Resume
             </h1>
             <div className="text-white">
               <p className="pb-5">
-                Hi, My name is Revathi. I am a Full stack web developer. I built
-                beautiful websites with React.js, Springboot , Bootstrap and
-                Tailwind CSS.
-              </p>
-              <p className="pb-5">
-                I am proficient in skills like React js, Tailwind CSS, Angular
-                js, Bootstrap, Thymeleaf, Springboot, Postman API, Restful web
-                services, Node.js, Mysql workbench, Jenkins, Aws basics, Docker,
-                Junit.
+                Hi here is my resume link, you can view my resume
+                <a className="button" href="#">
+                  Download
+                </a>
               </p>
             </div>
           </div>
