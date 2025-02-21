e0983ad2f48a00462ca890f16e17fcd857e37576
revathi <arevathi125@gmail.com>
2025-02-21 17:00:39 -0600

    committed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import booksImg from "../assets/book-shelf.avif";
 
 export default function Projects() {
   return (
-    <section className="flex py-20 px-5 justify-center bg-primary text-white">
+    <section className="flex-col py-20 px-5 justify-center bg-primary text-white">
       <div className="w-1/2">
         <div className="flex justify-center">
           <h1 className="text-4xl border-secondary border-b-4 mb-5 w-[140px] font-bold">
@@ -13,16 +13,16 @@ export default function Projects() {
       </div>
       <div className="w-1/2">
         <div className="flex px-10 gap-5">
-          <div>
+          <div className="relative">
             <img className="h-[200px]" src={bandImg} />
-            <div className="absolute left-0 right-0 bottom-0">
-              <p>Hi</p>
+            <div className="project-desc">
+              <p className="text-center py-5">Hi</p>
             </div>
           </div>
-          <div>
+          <div className="relative">
             <img className="h-[200px]" src={booksImg} />
-            <div>
-              <p>Hello</p>
+            <div className="project-desc">
+              <p className="text-center py-5">Hello</p>
             </div>
           </div>
         </div>
