fefbb3ad575cf9e55010fa1355fa534f7c047723
revathi <arevathi125@gmail.com>
2025-02-24 20:42:27 -0600

    committed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,16 +2,17 @@ export default function Contact() {
   return (
     <>
       <section className="flex flex-col bg-primary px-5 py-25">
-        <div className="flex flex-col items-center">
-          <h1 className="text-4xl text-white border-secondary border-b-4 mb-5 w-[130px] font-bold">
+        <div className="flex flex-col items-center text-white">
+          <h1 className="text-4xl border-secondary border-b-4 mb-5 w-[130px] font-bold">
             Contact
           </h1>
-          <p className="pb-5 text-white">
+          <p className="pb-5">
             If you want to discuss more in detail, please contact me
           </p>
           <p className="py-2">
             <span className="font-bold">Email: </span>arevathi125@gmail.com
-            <br />
+          </p>
+          <p className="py-2">
             <span className="font-bold">Phone: </span>+1 636-312-7473
           </p>
         </div>
