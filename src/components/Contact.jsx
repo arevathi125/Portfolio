4a75b57c3ebab9af12118a9cd06af9f0cf9e5e31
revathi <arevathi125@gmail.com>
2025-02-24 20:17:03 -0600

    committed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,20 +2,17 @@ export default function Contact() {
   return (
     <>
       <section className="flex flex-col md:flex-row bg-secondary">
-        <div className="md:w-1/2 flex justify-center">
-          <div className="flex flex-col justify-center px-5 py-142">
-            <h1 className="text-4xl border-primary text-white border-b-4 mb-5 w-[130px] font-bold">
-              Resume
-            </h1>
-            <div className="text-white">
-              <p className="pb-5">
-                Hi here is my resume link, you can view my resume
-                <a className="button" href="#">
-                  Download
-                </a>
-              </p>
-            </div>
-          </div>
+        <div className="flex justify-center">
+          <h1 className="text-4xl border-primary text-white border-b-4 mb-5 w-[130px] font-bold">
+            Resume
+          </h1>
+
+          <p className="pb-5">
+            Hi here is my resume link, you can view my resume
+            <a className="button" href="#">
+              Download
+            </a>
+          </p>
         </div>
       </section>
     </>
