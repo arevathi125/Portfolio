03b03d491bf3ba06801b2e4b3f27a770d011835f
revathi <arevathi125@gmail.com>
2025-02-26 15:03:44 -0600

    committed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Resume from "./components/Resume";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Projects />
       <Resume />
       <Contact />
+      <Footer />
     </div>
   );
 }
